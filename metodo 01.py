7b2de32386799ce40cea39840bb24429e73dfd98
IgorLimaKI <igorlimacmp@gmail.com>
2020-04-01 20:18:49 -0300

    Detector de bordas

--- metodo 01.py
+++ metodo 01.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 arquivo = open('nome.txt', 'w')
 
@@ -18,8 +19,53 @@ def output_image(name):
     plt.imshow(name)
     plt.show()
 
+def detectarBordas(img):
+    M, N = img.shape
+    imagemResultado = np.zeros((M, N))
+
+    for i in range(1, M - 1):
+        for j in range(1, N - 1):
+            corAnterior = img[i][j - 1]
+            if corAnterior > 128:
+                corAnterior = 1
+            else:
+                corAnterior = 0
+            corPosterior = img[i][j + 1]
+            if corPosterior > 128:
+                corPosterior = 1
+            else:
+                corPosterior = 0
+            derivada = corPosterior - corAnterior
+            if derivada == 1:
+                imagemResultado[i][j] = 1
+            else:
+                imagemResultado[i][j] = 0
+
+    for j in range(1, N - 1):
+        for i in range(1, M - 1):
+            corAnterior = img[i - 1][j]
+            if corAnterior > 128:
+                corAnterior = 1
+            else:
+                corAnterior = 0
+            corPosterior = img[i + 1][j]
+            if corPosterior > 128:
+                corPosterior = 1
+            else:
+                corPosterior = 0
+            derivada = corPosterior - corAnterior
+            if derivada == 1:
+                imagemResultado[i][j] = 1
+
+    return imagemResultado
 
 # abrindo imagem colorida e convertendo imagem para tom de cinza
-img = Image.open('cubo_2.jpg').convert('L')
+img = Image.open('circle.png').convert('L')
+img.save("out.jpg", "JPEG", quality=80, optimize=True, progressive=True)
 
-percorrerMatriz(np.array(img))
+image = mpimg.imread("out.jpg")
+percorrerMatriz(np.array(image))
+
+image = detectarBordas(image)
+
+output_image(image)
